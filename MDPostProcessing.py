2fe8355a577f7356a55c6d6cd42874bcedea1fbd
EmicoBinsfinder <egheosao@gmail.com>
2024-04-18 11:47:33 +0100

    modified:   MDPostProcessing.py 	new file:   Stress_AVGOnespd_bis_2_ethylhexyl_sebacate_T313KP1atm.out 	new file:   Stress_AVGOnespd_squalane_T373KP1atm.out 	modified:   avg_acf.csv 	modified:   viscosity_Einstein.csv 	modified:   viscosity_GK.csv

--- MDPostProcessing.py
+++ MDPostProcessing.py
@@ -1,205 +1,231 @@
 import sys, argparse
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from scipy import integrate
 from scipy.constants import Boltzmann
 from scipy.optimize import curve_fit
 from tqdm import trange
 from os.path import join as join
+from os import chdir
+from os import getcwd
+from os import listdir
+import os
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Define ACF
-def acf_(data):
-    steps = data.shape[0]
-    size = steps // 2
+chdir('C:/Users/eeo21/Desktop/Trajectory_Studies_1000')
+STARTDIR = getcwd()
 
-    autocorrelation = np.zeros(size, dtype=float)
-    for shift in trange(size, ncols=100, desc='Progress'):
-            autocorrelation[shift] = np.mean( (data[:steps-shift]) * (data[shift:]) )
+Names = [x for x in listdir(getcwd()) if os.path.isdir(x)]
+Temps = [313, 373]
 
-    return autocorrelation
+for Name in Names:
+    for Temp in Temps:
+            chdir(join(STARTDIR, Name))
+            Runs = [x for x in listdir(getcwd()) if os.path.isdir(x)]
+            print(Runs)
+            for Run in Runs:
+                try:
+                    chdir(join(STARTDIR, Name, Run))
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Define ACF
+                    def acf_(data):
+                        steps = data.shape[0]
+                        size = steps // 2
 
-# Define ACF using FFT
-def acf(data):
-    steps = data.shape[0]
-    lag = steps // 2
+                        autocorrelation = np.zeros(size, dtype=float)
+                        for shift in trange(size, ncols=100, desc='Progress'):
+                                autocorrelation[shift] = np.mean( (data[:steps-shift]) * (data[shift:]) )
 
-    # Nearest size with power of 2 (for efficiency) to zero-pad the input data
-    size = 2 ** np.ceil(np.log2(2 * steps - 1)).astype('int')
+                        return autocorrelation
 
-    # Compute the FFT
-    FFT = np.fft.fft(data, size)
+                    # Define ACF using FFT
+                    def acf(data):
+                        steps = data.shape[0]
+                        lag = steps // 2
 
-    # Get the power spectrum
-    PWR = FFT.conjugate() * FFT
+                        # Nearest size with power of 2 (for efficiency) to zero-pad the input data
+                        size = 2 ** np.ceil(np.log2(2 * steps - 1)).astype('int')
 
-    # Calculate the auto-correlation from inverse FFT of the power spectrum
-    COR = np.fft.ifft(PWR)[:steps].real
+                        # Compute the FFT
+                        FFT = np.fft.fft(data, size)
 
-    autocorrelation = COR / np.arange(steps, 0, -1)
+                        # Get the power spectrum
+                        PWR = FFT.conjugate() * FFT
 
-    return autocorrelation[:lag]
+                        # Calculate the auto-correlation from inverse FFT of the power spectrum
+                        COR = np.fft.ifft(PWR)[:steps].real
 
-df = pd.read_csv('Stress_AVGOnespd_squalane_T313KP1atm.out')
-# print(df.columns)
-# df = df.drop(columns=['TimeStep'])
-# print(df)
+                        autocorrelation = COR / np.arange(steps, 0, -1)
 
-unit = 'atm' #Pressure, bar or temperature
-datafile = 'Stress_AVGOnespd_squalane_T313KP1atm.out'
-diag = False # Use the diagonal components for viscosity prediction
-steps = len(df) - 5 # Num steps to read from the pressure tensor file
-timestep = 1 # What timestep are you using in the pressure tensor file
-temperature = 313 #System temp
-volume = 176163.49
-each = 100 # Sample frequency
-plot = True # Create plot for autocorrelation function
+                        return autocorrelation[:lag]
 
-# Conversion ratio from atm/bar to Pa
-if unit == 'Pa':
-    conv_ratio = 1
-elif unit == 'atm':
-    conv_ratio = 101325
-elif unit == 'bar':
-    conv_ratio = 100000
+                    df = pd.read_csv(f'Stress_AVGOnespd_{Name}_T{Temp}KP1atm.out')
+                    # print(df.columns)
+                    # df = df.drop(columns=['TimeStep'])
+                    # print(df)
 
-# Calculate the kBT value
-kBT = Boltzmann * temperature
+                    unit = 'atm' #Pressure, bar or temperature
+                    datafile = f'Stress_AVGOnespd_{Name}_T{Temp}KP1atm.out'
+                    diag = False # Use the diagonal components for viscosity prediction
+                    steps = len(df) - 5 # Num steps to read from the pressure tensor file
+                    timestep = 1 # What timestep are you using in the pressure tensor file
+                    temperature = 313 #System temp
+                    volume = 176163.49
+                    each = 100 # Sample frequency
+                    plot = True # Create plot for autocorrelation function
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Initiate the pressure tensor component lists
-Pxx, Pyy, Pzz, Pxy, Pxz, Pyz = [], [], [], [], [], []
+                    # Conversion ratio from atm/bar to Pa
+                    if unit == 'Pa':
+                        conv_ratio = 1
+                    elif unit == 'atm':
+                        conv_ratio = 101325
+                    elif unit == 'bar':
+                        conv_ratio = 100000
 
-# Read the pressure tensor elements from data file
-print('\nReading the pressure tensor data file')
-with open(datafile, "r") as file:
-    next(file)
-    next(file)
+                    # Calculate the kBT value
+                    kBT = Boltzmann * temperature
 
-    for _ in trange(steps, ncols=100, desc='Progress'):
-        line = file.readline()
-        step = list(map(float, line.split()))
-        Pxx.append(step[1]*conv_ratio)
-        Pyy.append(step[2]*conv_ratio)
-        Pzz.append(step[3]*conv_ratio)
-        Pxy.append(step[4]*conv_ratio)
-        Pxz.append(step[5]*conv_ratio)
-        Pyz.append(step[6]*conv_ratio)
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Initiate the pressure tensor component lists
+                    Pxx, Pyy, Pzz, Pxy, Pxz, Pyz = [], [], [], [], [], []
 
-# Convert lists to numpy arrays
-Pxx = np.array(Pxx)
-Pyy = np.array(Pyy)
-Pzz = np.array(Pzz)
-Pxy = np.array(Pxy)
-Pxz = np.array(Pxz)
-Pyz = np.array(Pyz)
+                    # Read the pressure tensor elements from data file
+                    print('\nReading the pressure tensor data file')
+                    with open(datafile, "r") as file:
+                        next(file)
+                        next(file)
 
-# Generate the time array
-end_step = steps * timestep
-Time = np.linspace(0, end_step, num=steps, endpoint=False)
+                        for _ in trange(steps, ncols=100, desc='Progress'):
+                            line = file.readline()
+                            step = list(map(float, line.split()))
+                            Pxx.append(step[1]*conv_ratio)
+                            Pyy.append(step[2]*conv_ratio)
+                            Pzz.append(step[3]*conv_ratio)
+                            Pxy.append(step[4]*conv_ratio)
+                            Pxz.append(step[5]*conv_ratio)
+                            Pyz.append(step[6]*conv_ratio)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Viscosity from Einstein relation
-def einstein(timestep):
+                    # Convert lists to numpy arrays
+                    Pxx = np.array(Pxx)
+                    Pyy = np.array(Pyy)
+                    Pzz = np.array(Pzz)
+                    Pxy = np.array(Pxy)
+                    Pxz = np.array(Pxz)
+                    Pyz = np.array(Pyz)
 
-    Pxxyy = (Pxx - Pyy) / 2
-    Pyyzz = (Pyy - Pzz) / 2
+                    # Generate the time array
+                    end_step = steps * timestep
+                    Time = np.linspace(0, end_step, num=steps, endpoint=False)
 
-    '''
-    Calculate the viscosity from the Einstein relation 
-    by integrating the components of the pressure tensor
-    '''
-    timestep = timestep * 10**(-12)
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Viscosity from Einstein relation
+                    def einstein(timestep):
 
-    Pxy_int = integrate.cumtrapz(y=Pxy, dx=timestep, initial=0)
-    Pxz_int = integrate.cumtrapz(y=Pxz, dx=timestep, initial=0)
-    Pyz_int = integrate.cumtrapz(y=Pyz, dx=timestep, initial=0)
+                        Pxxyy = (Pxx - Pyy) / 2
+                        Pyyzz = (Pyy - Pzz) / 2
 
-    Pxxyy_int = integrate.cumtrapz(y=Pxxyy, dx=timestep, initial=0)
-    Pyyzz_int = integrate.cumtrapz(y=Pyyzz, dx=timestep, initial=0)
+                        '''
+                        Calculate the viscosity from the Einstein relation 
+                        by integrating the components of the pressure tensor
+                        '''
+                        timestep = timestep * 10**(-12)
 
-    integral = (Pxy_int**2 + Pxz_int**2 + Pyz_int**2 + Pxxyy_int**2 + Pyyzz_int**2) / 5
+                        Pxy_int = integrate.cumtrapz(y=Pxy, dx=timestep, initial=0)
+                        Pxz_int = integrate.cumtrapz(y=Pxz, dx=timestep, initial=0)
+                        Pyz_int = integrate.cumtrapz(y=Pyz, dx=timestep, initial=0)
 
-    viscosity = integral[1:] * ( volume * 10**(-30) / (2 * kBT * Time[1:] * 10**(-12)) )
+                        Pxxyy_int = integrate.cumtrapz(y=Pxxyy, dx=timestep, initial=0)
+                        Pyyzz_int = integrate.cumtrapz(y=Pyyzz, dx=timestep, initial=0)
 
-    return viscosity
+                        integral = (Pxy_int**2 + Pxz_int**2 + Pyz_int**2 + Pxxyy_int**2 + Pyyzz_int**2) / 5
 
-viscosity = einstein(timestep=timestep)
+                        viscosity = integral[1:] * ( volume * 10**(-30) / (2 * kBT * Time[1:] * 10**(-12)) )
 
-print(f"\nViscosity (Einstein): {round((viscosity[-1] * 1000), 2)} [mPa.s]")
+                        return viscosity
 
-# Plot the running integral of viscosity
-if plot:
-    pyplot.figure()
-    pyplot.plot(Time[:viscosity.shape[0]], viscosity[:]*1000, label='Viscosity')
-    pyplot.xlabel('Time (ps)')
-    pyplot.ylabel('Viscosity (mPa.s)')
-    pyplot.legend()
-    pyplot.show()
+                    viscosity = einstein(timestep=timestep)
 
-# Save the running integral of viscosity as a csv file
-df = pd.DataFrame({"time(ps)" : Time[:viscosity.shape[0]:each], "viscosity(Pa.s)" : viscosity[::each]})
-df.to_csv("viscosity_Einstein.csv", index=False)
+                    print(f"\nViscosity (Einstein): {round((viscosity[-1] * 1000), 2)} [mPa.s]")
+                    viscE = round((viscosity[-1] * 1000), 2)
+                    print(viscE)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Viscosity from Green-Kubo relation
-def green_kubo(timestep):
-    # Calculate the ACFs
-    Pxy_acf = acf(Pxy)
-    Pxz_acf = acf(Pxz)
-    Pyz_acf = acf(Pyz)
+                    # Plot the running integral of viscosity
+                    if plot:
+                        plt.figure()
+                        plt.plot(Time[:viscosity.shape[0]], viscosity[:]*1000, label='Viscosity')
+                        plt.xlabel('Time (ps)')
+                        plt.ylabel('Einstein Viscosity (mPa.s)')
+                        plt.legend([f'Viscosity Estimate: {viscE} [mPa.s]'])
+                        plt.title(f'{Name}_{Run}_{Temp}_Einstein')
+                        plt.savefig(join(STARTDIR, Name, f'{Name}_{Run}_{Temp}_Einstein.png'))
 
-    # Calculate the shear components of the pressure tensor and their ACF
-    if diag:
-        Pxxyy = (Pxx - Pyy) / 2
-        Pyyzz = (Pyy - Pzz) / 2
-        Pxxzz = (Pxx - Pzz) / 2
+                    # Save the running integral of viscosity as a csv file
+                    # df = pd.DataFrame({"time(ps)" : Time[:viscosity.shape[0]:each], "viscosity(Pa.s)" : viscosity[::each]})
+                    # df.to_csv("viscosity_Einstein.csv", index=False)
 
-        Pxxyy_acf = acf(Pxxyy)
-        Pyyzz_acf = acf(Pyyzz)
-        Pxxzz_acf = acf(Pxxzz)
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Viscosity from Green-Kubo relation
+                    def green_kubo(timestep):
+                        # Calculate the ACFs
+                        Pxy_acf = acf(Pxy)
+                        Pxz_acf = acf(Pxz)
+                        Pyz_acf = acf(Pyz)
 
-    if diag:
-        avg_acf = (Pxy_acf + Pxz_acf + Pyz_acf + Pxxyy_acf + Pyyzz_acf + Pxxzz_acf) / 6
-    else:
-        avg_acf = (Pxy_acf + Pxz_acf + Pyz_acf) / 3
+                        # Calculate the shear components of the pressure tensor and their ACF
+                        if diag:
+                            Pxxyy = (Pxx - Pyy) / 2
+                            Pyyzz = (Pyy - Pzz) / 2
+                            Pxxzz = (Pxx - Pzz) / 2
 
-    # Integrate the average ACF to get the viscosity
-    timestep = timestep * 10**(-12)
-    integral = integrate.cumtrapz(y=avg_acf, dx=timestep, initial=0)
-    viscosity = integral * (volume * 10**(-30) / kBT)
+                            Pxxyy_acf = acf(Pxxyy)
+                            Pyyzz_acf = acf(Pyyzz)
+                            Pxxzz_acf = acf(Pxxzz)
 
-    return avg_acf, viscosity
+                        if diag:
+                            avg_acf = (Pxy_acf + Pxz_acf + Pyz_acf + Pxxyy_acf + Pyyzz_acf + Pxxzz_acf) / 6
+                        else:
+                            avg_acf = (Pxy_acf + Pxz_acf + Pyz_acf) / 3
 
-avg_acf, viscosity = green_kubo(timestep)
+                        # Integrate the average ACF to get the viscosity
+                        timestep = timestep * 10**(-12)
+                        integral = integrate.cumtrapz(y=avg_acf, dx=timestep, initial=0)
+                        viscosity = integral * (volume * 10**(-30) / kBT)
 
-# Plot the normalized average ACF
-if plot:
-    norm_avg_acf = avg_acf / avg_acf[0]
-    pyplot.figure()
-    pyplot.plot(Time[:avg_acf.shape[0]], norm_avg_acf[:], label='Average')
-    pyplot.xlabel('Time (ps)')
-    pyplot.ylabel('ACF')
-    pyplot.legend()
-    pyplot.show()
+                        return avg_acf, viscosity
 
-# Save the normalized average ACF as a csv file
-norm_avg_acf = avg_acf / avg_acf[0]
-df = pd.DataFrame({"time (ps)" : Time[:avg_acf.shape[0]], "ACF" : norm_avg_acf[:]})
-df.to_csv("avg_acf.csv", index=False)
+                    avg_acf, viscosity = green_kubo(timestep)
 
-print(f"Viscosity (Green-Kubo): {round((viscosity[-1] * 1000), 2)} [mPa.s]")
+                    # Plot the normalized average ACF
+                    if plot:
+                        norm_avg_acf = avg_acf / avg_acf[0]
+                        plt.figure()
+                        plt.plot(Time[:avg_acf.shape[0]], norm_avg_acf[:], label='Average')
+                        plt.xlabel('Time (ps)')
+                        plt.ylabel('Green Kubo ACF')
+                        plt.legend()
+                        plt.title(f'{Name}_{Run}_{Temp}_GKACF')
+                        plt.savefig(join(STARTDIR, Name, f'{Name}_{Run}_{Temp}_GKACF.png'))
 
-# Plot the time evolution of the viscosity estimate
-if plot:
-    pyplot.figure()
-    pyplot.plot(Time[:viscosity.shape[0]], viscosity[:]*1000, label='Viscosity')
-    pyplot.xlabel('Time (ps)')
-    pyplot.ylabel('Viscosity (mPa.s)')
-    pyplot.legend()
-    pyplot.show()
+                    # Save the normalized average ACF as a csv file
+                    norm_avg_acf = avg_acf / avg_acf[0]
+                    # df = pd.DataFrame({"time (ps)" : Time[:avg_acf.shape[0]], "ACF" : norm_avg_acf[:]})
+                    # df.to_csv("avg_acf.csv", index=False)
 
-# Save running integral of the viscosity as a csv file
-df = pd.DataFrame({"time(ps)" : Time[:viscosity.shape[0]:each], "viscosity(Pa.s)" : viscosity[::each]})
-df.to_csv("viscosity_GK.csv", index=False)
+                    print(f"Viscosity (Green-Kubo): {round((viscosity[-1] * 1000), 2)} [mPa.s]")
+
+                    # Plot the time evolution of the viscosity estimate
+                    if plot:
+                        plt.figure()
+                        plt.plot(Time[:viscosity.shape[0]], viscosity[:]*1000, label='Viscosity')
+                        plt.xlabel('Time (ps)')
+                        plt.ylabel('Green Kubo Viscosity (mPa.s)')
+                        plt.legend([f'Viscosity Estimate: {round((viscosity[-1] * 1000), 2)} [mPa.s]'])
+                        plt.title(f'{Name}_{Run}_{Temp}_GK')
+                        plt.savefig(join(STARTDIR, f'{Name}_{Run}_{Temp}_GreenKubo.png'))
+
+                    # Save running integral of the viscosity as a csv file
+                    df = pd.DataFrame({"time(ps)" : Time[:viscosity.shape[0]:each], "viscosity(Pa.s)" : viscosity[::each]})
+                    # df.to_csv("viscosity_GK.csv", index=False)
+                except Exception as E:
+                    print(E)
+                    pass
